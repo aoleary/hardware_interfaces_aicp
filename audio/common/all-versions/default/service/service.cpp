f33943f36ba4756b62ebca6a0dc0a6e1f470fd4e
TARKZiM <tom8476oo@gmail.com>
2022-02-12 19:16:35 +0800

    audio: Bring back 2.0 hal support
    Change-Id: Ib70c271393f2079f9ff60b442ee3ee431b15b3c2

--- audio/common/all-versions/default/service/service.cpp
+++ audio/common/all-versions/default/service/service.cpp
@@ -71,6 +71,7 @@ int main(int /* argc */, char* /* argv */ []) {
             "android.hardware.audio@6.0::IDevicesFactory",
             "android.hardware.audio@5.0::IDevicesFactory",
             "android.hardware.audio@4.0::IDevicesFactory",
+            "android.hardware.audio@2.0::IDevicesFactory"
         },
         {
             "Audio Effect API",
@@ -78,6 +79,7 @@ int main(int /* argc */, char* /* argv */ []) {
             "android.hardware.audio.effect@6.0::IEffectsFactory",
             "android.hardware.audio.effect@5.0::IEffectsFactory",
             "android.hardware.audio.effect@4.0::IEffectsFactory",
+            "android.hardware.audio.effect@2.0::IEffectsFactory",
         }
     };
 
